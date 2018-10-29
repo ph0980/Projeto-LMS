918f42c3bd3cd8d58a54bb8940bb27d62e49b7a1
Pedro Henrique <38732869+ph0980@users.noreply.github.com>
2018-10-29 10:38:29 -0300

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'u1u5)2iur-g-6kyz522a3eg5q_@3c0d!5+3$=a72hu0sq8c$31'
 DEBUG = True
 
 #alterei isso aqui
-ALLOWED_HOSTS = ['lms-proj-2018.azurewebsites.net']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lms-proj-2018.azurewebsites.net','']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
