b01581854002cef6822a748167961979b8629c0a
enp256 <ericknp2010@hotmail.com>
2018-11-08 09:18:59 -0200

    'settings'

--- lms/settings.py
+++ lms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u1u5)2iur-g-6kyz522a3eg5q_@3c0d!5+3$=a72hu0sq8c$31'
 DEBUG = True
 
 #host do azure
-ALLOWED_HOSTS = ['lms-proj-2018.azurewebsites.net','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['lms-proj-2018.azurewebsites.net']
 
 
 
